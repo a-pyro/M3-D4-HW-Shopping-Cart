574eaf4f6506fff282f8e04ece99409d4d1e5512
ardigermenji <ardi.germenji@gmail.com>
2021-03-11 22:49:58 +0100

    adds filter feature

--- app.js
+++ app.js
@@ -6,32 +6,15 @@ const shit = '💩',
   rocket = '🚀',
   poudzo = '👍🏻';
 
-/*             You are creating the "shopping cart experience" for a Online Marketplace.
-            You have the list of the available books from the current API:
-            https://striveschool-api.herokuapp.com/books
-            What you have to do is:
-           XX 0) Get all the products from the API using a fetch
-           XX 1) Display the list of items available on the page using template literals `` and .forEach
-           XX 2) Add a "add to cart button"
-            3) When the button is pressed, change the style of the item and add it to another list
-           XX 4) Add "skip" button next to each item
-           XX 5) When pressed, the button should remove from the page the item not interesting from the user
-            6) Add a "search bar". When the user types more than 3 chars, you should filter the content of the page to show only the items with a matching name (hint: use .filter method)
-            7) Allow the user to delete items from the cart list
-            
-            [EXTRA]
-            8) Add a "clean cart" button, to clean the whole list.
-            9) Create a second "detail page" for the product. When the user clicks on a product name, the app should redirect him to the secondary page, passing the ASIN in query string
-            10) In page "detail" show some details of the selected product (https://striveschool-api.herokuapp.com/books/1940026091 to fetch the details of a specific book) */
-
 let userCart = [];
 const entryPoint = document.getElementById('entryPoint');
 const shoppingCartDom = document.getElementById('shoppingCartDom');
 const totalItems = document.getElementById('totalItems');
 const totalAmount = document.getElementById('totalAmount');
+const searchField = document.querySelector('.form-inline input');
 
 window.addEventListener('DOMContentLoaded', getData);
-
+searchField.addEventListener('keyup', filterBooks);
 async function getData() {
   try {
     const res = await fetch('https://striveschool-api.herokuapp.com/books');
@@ -45,13 +28,18 @@ async function getData() {
 }
 
 function renderData(data) {
-  data.forEach((book) => {
+  entryPoint.innerHTML = data.reduce((prev, book) => {
+    const { asin, category, img: url, price, title } = book;
+    return prev + CardComponent(asin, category, url, price, title);
+  }, '');
+
+  /* data.forEach((book) => {
     const { asin, category, img: url, price, title } = book;
     entryPoint.insertAdjacentHTML(
       'beforeend',
       CardComponent(asin, category, url, price, title)
     );
-  });
+  }); */
 }
 
 function CardComponent(asin, category, url, price, title) {
@@ -167,3 +155,25 @@ $('#shoppingCartDom').on('click', function (event) {
   // therefore delegated events won't be fired
   event.stopPropagation();
 });
+
+function filterBooks(e) {
+  const query = e.target.value;
+
+  if (query.length > 3) {
+    if (entryPoint.children.length === 1) return;
+    console.log(query);
+    fetch('https://striveschool-api.herokuapp.com/books')
+      .then((res) => res.json())
+      .then((data) => {
+        const filteredData = data.filter((book) =>
+          book.title.toLowerCase().includes(query.toLowerCase())
+        );
+        console.log(filteredData);
+        renderData(filteredData);
+        addCardBtnsEventListeners();
+      })
+      .catch(console.log);
+  }
+
+  if (!query) getData();
+}
